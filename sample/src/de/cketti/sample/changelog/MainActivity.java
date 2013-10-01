ec9fc6658d0661c6b9ee0625f8dffff1ee9c3808
cketti <cketti@gmail.com>
2013-10-01 04:03:41 +0200

    Extend sample to show how to display themed dialogs
    Displaying a themed ChangeLog dialog is as easy as passing a
    themed context instance and matching CSS rules to the
    constructor.

--- sample/src/de/cketti/sample/changelog/MainActivity.java
+++ sample/src/de/cketti/sample/changelog/MainActivity.java
@@ -1,8 +1,10 @@
 package de.cketti.sample.changelog;
 
 import de.cketti.library.changelog.ChangeLog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.ContextThemeWrapper;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -30,7 +32,7 @@ public class MainActivity extends FragmentActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_whats_new: {
-                new ChangeLog(this).getLogDialog().show();
+                new DarkThemeChangeLog(this).getLogDialog().show();
                 break;
             }
             case R.id.menu_full_changelog: {
@@ -40,5 +42,17 @@ public class MainActivity extends FragmentActivity {
         }
 
         return true;
+    }
+
+    /**
+     * Example that shows how to create a themed dialog.
+     */
+    public static class DarkThemeChangeLog extends ChangeLog {
+        public static final String DARK_THEME_CSS =
+                "body { color: #ffffff; background-color: #282828; }" + "\n" + DEFAULT_CSS;
+
+        public DarkThemeChangeLog(Context context) {
+            super(new ContextThemeWrapper(context, R.style.DarkTheme), DARK_THEME_CSS);
+        }
     }
 }
